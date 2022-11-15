2646e34ecd497cd65cf8c7a6964ce22c6d5e8411
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-15 15:47:52 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>rapiddweller | Driving digital transformation</title>
+        <meta
+          name="description"
+          content="Benerator is the smart way to generate, migrate, obfuscate (anonymize / pseudonymize) data for testing based on easy to maintain datamodels."
+        ></meta>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://www.benerator.de/" />
+        <title>
+          Benerator | Generate, obfuscate &amp; migrate test data smart | Faker
+        </title>
+      </Head>
+
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4"></main>
+    </>
+  );
+};
+
+export default Home;
