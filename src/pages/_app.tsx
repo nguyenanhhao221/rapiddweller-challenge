5b1b84ed349fa7fae3821807e144f816c50f1da2
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-17 05:11:09 +0700

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import Script from "next/script";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-K74HTLPG33`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K74HTLPG33', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default MyApp;
